09313a3a2f233e972e5d7e68307910acc55e803d
algarsa <alice.santos.st@gmail.com>
2024-08-15 10:17:12 -0300

    first commit

--- cypress/e2e/cypress_api_tc.cy.js
+++ cypress/e2e/cypress_api_tc.cy.js
@@ -1,16 +1,24 @@
 /// <reference types="cypress" />
 
-describe('Productos api', () => {
-    context('GET /api/v1/products/search', () => {
+describe('Products api', () => {
+    context('GET /catalog/api/v1/products/search?name=hp pro', () => {
         it('Deve retornar produto HP Pro Tablet 608', () => {
             cy.request({
                 method: 'GET',
-                url: 'https://www.advantageonlineshopping.com/catalog/api/v1/products/search?name=hp pro&quantityPerEachCategory=-1'
+                url: 'https://www.advantageonlineshopping.com/catalog/api/v1/products/search?name=hp pro'
             })
                 .then((response) => {
-                    expect(response.status).to.be.equal(200);
-                    expect(response.body).is.not.empty;
                     cy.log(JSON.stringify(response.body));
+                    expect(response.status).to.be.equal(200);
+                    expect(response.body[0].products[0]).is.not.empty;
+                    expect(response.body[0].products[0]).to.have.all.keys(
+                        'productId', 'categoryId', 'productName', 'price', 'imageUrl'
+                    );
+                    expect(response.body[0].products[0].productId).to.eq(18);
+                    expect(response.body[0].products[0].categoryId).to.eq(3);
+                    expect(response.body[0].products[0].productName).to.eq("HP Pro Tablet 608 G1");
+                    expect(response.body[0].products[0].price).to.eq(479);
+                    expect(response.body[0].products[0].imageUrl).to.eq('3300');
                 })
         });
     });
